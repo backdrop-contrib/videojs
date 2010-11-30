14e888a2520f2eeedcb12a22db59b75f6031fc26
Heshan Wanigasooriya <heshanmw@gmail.com>
2010-11-30 20:40:00 +0000

    #976400 by ull no flash fallback.

--- theme/videojs.tpl.php
+++ theme/videojs.tpl.php
@@ -17,12 +17,50 @@
     <?php $mimetype = $item['filemime']; ?>
     <?php if (array_key_exists($mimetype, $codecs)): ?>
     <?php $mimetype = ($mimetype == 'video/quicktime') ? 'video/mp4' : $mimetype; ?>
+    <?php if($mimetype == 'video/mp4' || $mimetype == 'video/flv') $flash =  $filepath;?>
     <?php $videojs_sources .= "<source src=\"$filepath\" type='$mimetype; codecs=\"" . $codecs[$mimetype] . "\"' />"; ?>
     <?php endif; ?>
     <?php endforeach; ?>
     <?php print $videojs_sources; ?>
-    <!-- Flash Fallback. Use any flash video player here. Make sure to keep the vjs-flash-fallback class. -->
-    <!-- @TODO: Add Flowplayer on SWFTools or Flowplayer API modules -->
+        <!-- Flash Fallback. Use any flash video player here. Make sure to keep the vjs-flash-fallback class. -->
+    <?php
+        if (module_exists('swftools')) {
+          $options = array(
+            'params' => array(
+              'width' => variable_get('videojs_width', 640),
+              'height' => variable_get('videojs_height', 264),
+            ),
+            'othervars' => array(
+              //@todo: swftools bug, can't enable this until they fix their pathing for the images.
+              'image' => $poster,
+            ),
+          );
+          $themed_output = swf($flash, $options);
+        } elseif (module_exists('flowplayer')) {
+          // kjh: use a playlist to display the thumbnail if not auto playing
+          if (!empty($poster)) {
+            $options = array(
+              'playlist' => array($poster,
+                array('url' => urlencode($flash),
+                  'autoPlay' => FALSE,
+                  'autoBuffering' => TRUE,
+                ),),);
+          } else {
+            $options = array(
+              'clip' => array('url' => urlencode($flash),
+                'autoPlay' => FALSE,
+                'autoBuffering' => TRUE,
+              ),);
+          }
+
+          $themed_output = theme(
+                  'flowplayer',
+                  $options,
+                  // adding 24px to height #973636
+                  array('style' => 'width:' . variable_get('videojs_width', 640) . 'px;height:' . variable_get('videojs_height', 264) + 24 . 'px;')
+          );
+        }
+    ?>
   </video>
 </div>
 <!-- End VideoJS -->
