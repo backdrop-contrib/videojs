2df620d26982c52ba4a585b9a26191a7ead620b0
Jorrit Schippers <jorrit@161217.no-reply.drupal.org>
2012-02-04 19:14:48 +0100

    Issue #1394206 by Jorrit: Fixed autoplay not working, HTML escaping in codec attribute of the source element, adding empty poster attribute.

--- theme/videojs.tpl.php
+++ theme/videojs.tpl.php
@@ -1,24 +1,30 @@
-<!-- Begin VideoJS -->
-<?php if (count($items) > 0): ?>
-  <div class="video-js-box <?php print variable_get('videojs_skin', 'default') ?>" id="<?php print $player_id; ?>">
-    <!-- Using the Video for Everybody Embed Code http://camendesign.com/code/video_for_everybody -->
-    <video id="<?php print $player_id; ?>-video" class="video-js" width="<?php print($width) ?>" height="<?php print($height) ?>" controls="controls" preload="auto" poster="<?php print($poster) ?>">
-      <?php foreach ($items as $item): ?>
-        <?php $filepath = file_create_url($item['uri']); ?>
-        <source src="<?php print(file_create_url($item['uri'])) ?>" type="<?php print($item['videotype']) ?>" />
-      <?php endforeach; ?>
-      <!-- Flash Fallback. Use any flash video player here. Make sure to keep the vjs-flash-fallback class. -->
-      <?php
-      if (!empty($flash_player)):
-        print($flash_player);
-      elseif(!empty($flash)):
-        ?>
-        <object class="vjs-flash-fallback" width="<?php print($width) ?>" height="<?php print($height) ?>" type="application/x-shockwave-flash"
-                data="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf">
-          <param name="movie" value="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf" />
-          <param name="allowfullscreen" value="true" />
-          <param name="flashvars" value="config={'playlist': [ <?php if(isset($poster)) print("'".$poster."',"); ?> {'url': '<?php print($flash) ?>', 'autoPlay':false, 'autoBuffering':true} ]}" />
-          <embed id="flash-<?php print $player_id; ?>"
+<?php
+/**
+ * Provide the HTML output of the videojs audio player.
+ */
+
+$attrs = '';
+if (!empty($autoplay)) {
+  $attrs .= ' autoplay="autoplay"';
+}
+if (!empty($poster)) {
+  $attrs .= ' poster="'. check_plain($poster) .'"';
+}
+
+if (!empty($items)): ?>
+<div class="video-js-box <?php print variable_get('videojs_skin', 'default') ?>" id="<?php print $player_id; ?>">
+  <video id="<?php print $player_id; ?>-video" class="video-js" width="<?php print($width) ?>" height="<?php print($height) ?>" controls="controls" preload="auto"<?php echo $attrs; ?>>
+<?php foreach ($items as $item): ?>
+    <source src="<?php print(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
+<?php endforeach; ?>
+<?php if (!empty($flash_player)): ?>
+    <div class="vjs-flash-fallback"><?php echo $flash_player; ?></div>
+<?php elseif(!empty($flash)): ?>
+    <object class="vjs-flash-fallback" width="<?php print($width) ?>" height="<?php print($height) ?>" type="application/x-shockwave-flash" data="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf">
+      <param name="movie" value="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf" />
+      <param name="allowfullscreen" value="true" />
+      <param name="flashvars" value="config={'playlist': [ <?php if(isset($poster)) print("'".$poster."',"); ?> {'url': '<?php print($flash) ?>', 'autoPlay':false, 'autoBuffering':true} ]}" />
+      <embed id="flash-<?php print $player_id; ?>"
                  name="flash-<?php print $player_id; ?>"
                  src="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf"
                  width="<?php print($width) ?>"
@@ -28,13 +34,11 @@
                  allowfullscreen='true'
                  flashvars="config={'playlist': [ <?php if(isset($poster)) print("'".$poster."',"); ?> {'url': '<?php print($flash) ?>', 'autoPlay':false, 'autoBuffering':true} ]}"
                  />
-          <!-- Image Fallback. Typically the same as the poster image. -->
-          <img src="<?php print($poster) ?>" width="<?php print($width) ?>" height="<?php print($height) ?>" alt="Poster Image"
-               title="No video playback capabilities." />
-        </object>
-      <?php endif; //$flash_player   ?>
-    </video>
-  </div>
+<?php if(!empty($poster)): ?>
+      <img src="<?php print($poster) ?>" width="<?php print($width) ?>" height="<?php print($height) ?>" alt="Poster Image" title="No video playback capabilities." />
 <?php endif; ?>
-<!-- End VideoJS -->
-
+    </object>
+<?php endif; ?>
+  </video>
+</div>
+<?php endif;
