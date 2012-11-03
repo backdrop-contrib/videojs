b2f6f50a15769c9823e0da5c726d2af705a3ca15
Jorrit Schippers <jorrit@161217.no-reply.drupal.org>
2012-11-03 16:20:26 +0100

    Properly encode video URLs.

--- theme/videojs.tpl.php
+++ theme/videojs.tpl.php
@@ -15,7 +15,7 @@ if (!empty($poster)) {
 if (!empty($items)): ?>
 <video id="<?php print $player_id; ?>-video" data-setup="{}" class="video-js vjs-default-skin" width="<?php print($width) ?>" height="<?php print($height) ?>" controls="controls" preload="auto"<?php echo $attrs; ?>>
 <?php foreach ($items as $item): ?>
-  <source src="<?php print(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
+  <source src="<?php print check_plain(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
 <?php endforeach; ?>
 </video>
 <?php endif;
