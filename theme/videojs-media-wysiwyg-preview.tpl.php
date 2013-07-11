111c523f5cad9171d0e8ed719017bebc0df8b993
Jorrit Schippers <jorrit@161217.no-reply.drupal.org>
2013-07-11 17:32:32 +0200

    Issue #2024815 by Jorrit: Added support for inline videos in wysiwyg editors using the Media module and added other compatibility fixes with the Media and File Entity modules.

---
+++ theme/videojs-media-wysiwyg-preview.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Provide a stub HTML output of the Video.js video player for WYSIWYG editors.
+ *
+ * Available variables:
+ * Same as videojs.tpl.php.
+ */
+?>
+ <img src="<?php print check_plain($poster); ?>" width="<?php print $width; ?>" height="<?php print $height; ?>" />
